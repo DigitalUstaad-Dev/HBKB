6440bf0b5688be1cb2fb77f2f577f088dc90d664
DigitalUstaad-Dev <maxtxt629@gmail.com>
2025-11-19 16:15:52 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { createApp } from "./app/server";
 
+const port = Number(process.env.PORT) || 3000;
+
+const app = createApp();
+
+app.listen(port, () => {
+  console.log(`[HBKB] API running on http://localhost:${port}`);
+});
