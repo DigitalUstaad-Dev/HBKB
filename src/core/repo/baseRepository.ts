39119c1acd22a685342690c09da0f513304968b9
DigitalUstaad-Dev <maxtxt629@gmail.com>
2025-11-19 16:27:03 +0500

    Update baseRepository.ts

--- src/core/repo/baseRepository.ts
+++ src/core/repo/baseRepository.ts
@@ -1,1 +1,9 @@
+// src/core/repo/baseRepository.ts
 
+export interface CrudRepository<T, TCreate, TUpdate> {
+  list(): Promise<T[]>;
+  findById(id: string): Promise<T | null>;
+  create(data: TCreate): Promise<T>;
+  update(id: string, data: TUpdate): Promise<T | null>;
+  delete(id: string): Promise<boolean>;
+}
