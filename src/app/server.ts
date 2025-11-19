921cfd35cfd9e358fd19000049821cb18c6c8e4e
DigitalUstaad-Dev <maxtxt629@gmail.com>
2025-11-19 16:16:17 +0500

    Update server.ts

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,1 +1,11 @@
+import express from "express";
+import { registerRoutes } from "./routes";
 
+export function createApp() {
+  const app = express();
+
+  app.use(express.json());
+  registerRoutes(app);
+
+  return app;
+}
