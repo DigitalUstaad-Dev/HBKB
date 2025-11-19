61d3be61fb8848c49085452f24443201fdf41f17
DigitalUstaad-Dev <maxtxt629@gmail.com>
2025-11-19 16:37:36 +0500

    Update service.ts

--- src/modules/leads/service.ts
+++ src/modules/leads/service.ts
@@ -1,1 +1,10 @@
+// src/modules/leads/service.ts
 
+import { createCrudService } from "../../core/service/baseService";
+import { Lead } from "./model";
+import { CreateLeadDto, UpdateLeadDto } from "./dto";
+import { leadRepository } from "./repository";
+
+export const leadService = createCrudService<Lead, CreateLeadDto, UpdateLeadDto>(
+  leadRepository
+);
